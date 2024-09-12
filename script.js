87f8621a216401e9a656fa5a53518e19790c3191
German Uriarte <uriarte.german@gmail.com>
2024-09-12 13:57:30 -0400

    added first row in grid

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const gridContainer = document.querySelector(".grid-container");
 const gridItem = document.createElement("div");
+gridItem.className = "grid-item";
+
+for (let i = 0; i < 16; i++) {
+  const gridItem = document.createElement("div");
+  gridItem.className = "grid-item";
+  gridContainer.appendChild(gridItem);
+}
