5d10cad6391868d3eba7b9c88a90ca431173c2ae
German Uriarte <uriarte.german@gmail.com>
2024-09-12 14:58:13 -0400

    added button for new grid

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+const newGridButton = document.querySelector(".reset-button");
 const gridContainer = document.querySelector(".grid-container");
+let numberOfRowsAndColumns = 16;
 
-for (let i = 0; i < 16 * 16; i++) {
-  const gridItem = document.createElement("div");
-  gridItem.className = "grid-item";
-  gridContainer.appendChild(gridItem);
-}
+generateGrid();
 
 document.querySelectorAll(".grid-item").forEach((gridItem) => {
   gridItem.addEventListener("mouseover", (e) => {
     e.currentTarget.style.backgroundColor = "black";
   });
 });
+
+newGridButton.addEventListener("click", () => {
+  numberOfRowsAndColumns = getRowsAndColumnsFromUser();
+});
+
+function generateGrid() {
+  for (let i = 0; i < 16 * 16; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.className = "grid-item";
+    gridContainer.appendChild(gridItem);
+  }
+}
+
+function getRowsAndColumnsFromUser() {
+  let response;
+
+  do {
+    response = prompt("How many rows and columns do you want? (max 100)");
+    if (response < 0 || response > 100) {
+      alert("Please select a number between 0 and 100. Try again.");
+    }
+  } while (response < 0 || response > 100);
+
+  return response;
+}
